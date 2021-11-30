50d617b4684559114f3568908858c9a36623fc7e
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-11-30 15:42:42 +0100

    Uppdaterat programmet med bara en klass.

--- Program.cs
+++ Program.cs
@@ -6,14 +6,11 @@ namespace StaffFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter name: ");
+            Console.WriteLine("Please enter the name: ");
             string name = Console.ReadLine();
-            Console.WriteLine("The name is: " + name);
-
             Console.WriteLine("Please enter the salary: ");
-            int salary = Convert.ToInt32(Console.ReadLine();
-            Console.WriteLine("The salary is: " + salary);
-
+            int salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The name and salary is: {name} {salary}");
         }
     }
 }
