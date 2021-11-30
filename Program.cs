650dac40bb03fb5e0cdd409bb0ccd1f4bacefdbb
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-11-30 14:54:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StaffFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
